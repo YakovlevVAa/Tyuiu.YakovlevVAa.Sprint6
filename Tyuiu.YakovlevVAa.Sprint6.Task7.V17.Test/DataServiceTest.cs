6f67235072706f4a513f3a5693433a55a8ff4cd1
Виктор Яковлев <Tyuiu.YakovlevVA@outlook.com>
2024-12-14 01:56:41 +0500

    7

--- Tyuiu.YakovlevVAa.Sprint6.Task7.V17.Test/DataServiceTest.cs
+++ Tyuiu.YakovlevVAa.Sprint6.Task7.V17.Test/DataServiceTest.cs
@@ -6,6 +6,11 @@ namespace Tyuiu.YakovlevVAa.Sprint6.Task7.V17.Test
         [TestMethod]
         public void TestMethod1()
         {
+            string path = @"C:\Users\victor\Downloads\InPutDataFileTask7V17.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
